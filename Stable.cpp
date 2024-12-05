538aa90cd159e04962cc3c8c70a47a30f348af99
Tikhon20043 <144627214+Tikhon20043@users.noreply.github.com>
2024-12-05 23:09:02 +0300

    Update Stable.cpp

--- Stable.cpp
+++ Stable.cpp
@@ -5,35 +5,35 @@ Stable::Stable() : horseCount(0), employeeCount(0), inventoryCount(0) {}
 
 void Stable::addHorse(const Horse& horse) { 
     if (horseCount < maxHorses) {
-        horses[horseCount++] = horse; // Добавляем лошадь в массив и увеличиваем счетчик
+        horses[horseCount++] = horse; 
     }
 }
 
 void Stable::addEmployee(const Groom& employee) { 
     if (employeeCount < maxEmployees) {
-        employees[employeeCount++] = employee; // Добавляем сотрудника в массив и увеличиваем счетчик
+        employees[employeeCount++] = employee; 
     }
 }
 
 void Stable::addInventoryItem(const InventoryItem& item) { 
     if (inventoryCount < maxInventoryItems) {
-        inventory[inventoryCount++] = item; // Добавляем элемент инвентаря в массив и увеличиваем счетчик
+        inventory[inventoryCount++] = item; 
     }
 }
 
 void Stable::displayAllInfo() const {
     std::cout << "\n--- Horses ---\n";
     for (int i = 0; i < horseCount; ++i) {
-        horses[i].displayInfo(); // Вызываем метод отображения информации о лошади
+        horses[i].displayInfo(); 
     }
 
     std::cout << "\n--- Employees ---\n";
     for (int i = 0; i < employeeCount; ++i) {
-        employees[i].displayInfo(); // Вызываем метод отображения информации о сотруднике
+        employees[i].displayInfo(); 
     }
 
     std::cout << "\n--- Inventory ---\n";
     for (int i = 0; i < inventoryCount; ++i) {
-        inventory[i].displayInfo(); // Вызываем метод отображения информации о элементе инвентаря
+        inventory[i].displayInfo(); 
     }
 }
