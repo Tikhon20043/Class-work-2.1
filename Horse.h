aab6a3192cf7d07367709afa481a4bd9a88af30f
Tikhon20043 <144627214+Tikhon20043@users.noreply.github.com>
2024-12-05 23:06:39 +0300

    Update Horse.h

--- Horse.h
+++ Horse.h
@@ -1,18 +1,18 @@
 #pragma once
 
-#include "Animal.h" // Подключаем класс Animal
-#include "IDisplayable.h" // Подключаем интерфейс IDisplayable
-#include <iostream> // Подключаем библиотеку для ввода-вывода
+#include "Animal.h" 
+#include "IDisplayable.h" 
+#include <iostream> 
 
-// Класс Horse наследует от Animal и IDisplayable
+
 class Horse : public Animal, public IDisplayable {
 public:
-    Horse() : Animal("Unnamed", 0), breed("Unknown") {} // Конструктор по умолчанию
+    Horse() : Animal("Unnamed", 0), breed("Unknown") {} 
     Horse(const std::string& name, const std::string& breed, int age)
         : Animal(name, age), breed(breed) {}
 
     void displayInfo() const override;
 
 private:
-    std::string breed; // Порода лошади
+    std::string breed; 
 };
